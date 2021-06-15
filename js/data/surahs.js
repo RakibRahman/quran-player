0f1ebfb7d5b86c3a53cd912af2601ed13e771e07
RakibRahman <md.rr.talukder@gmail.com>
2021-06-15 18:27:16 +0600

    init coomit v2

---
+++ js/data/surahs.js
@@ -0,0 +1,44 @@
+export const songsList = [
+  {
+    id: "1",
+    url: "../../music/nice-for-what.mp3",
+    time: "04:22",
+    title: "Nice For What",
+    artist: "Drake",
+  },
+  {
+    id: "2",
+    url: "../../music/gods-plan.mp3",
+    time: "03:19",
+    title: "God's Plan",
+    artist: "Drake",
+  },
+  {
+    id: "3",
+    url: "../../music/professional-rapper.mp3",
+    time: "05:52",
+    title: "Professional Rapper ft. Snoop Dog",
+    artist: "Lil Dicky",
+  },
+  {
+    id: "4",
+    url: "../../music/yellow.mp3",
+    time: "04:29",
+    title: "Yellow",
+    artist: "Coldplay",
+  },
+  {
+    id: "5",
+    url: "../../music/get-lucky.mp3",
+    time: "06:09",
+    title: "Get Lucky",
+    artist: "Daft Punk",
+  },
+  {
+    id: "6",
+    url: "../../music/shape-of-my-heart.mp3",
+    time: "04:43",
+    title: "Shape Of My Heart",
+    artist: "Sting",
+  },
+];
---
+++ js/modules/playlist.js
@@ -0,0 +1,9 @@
+const Playlist = ((_) => {
+  const init = (_) => {
+    console.log("working ok");
+  };
+  return {
+    init,
+  };
+})();
+export default Playlist;
